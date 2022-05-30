c2ac1aa84970a4fb114aa5f511f29e2ac9276854
mizuki <84909407+mmizuki1817@users.noreply.github.com>
2022-05-31 05:02:10 +0900

    Update ft_substr.c

--- ft_substr.c
+++ ft_substr.c
@@ -28,7 +28,7 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 	}
 	while (start-- > 0)
 		s++;
-	p = (char)malloc(sizeof(char) * (len + 1));
+	p = (char *)malloc(sizeof(char) * (len + 1));
 	if (p == NULL)
 		return (NULL);
 	ft_strlcpy(p, s, (len + 1));
