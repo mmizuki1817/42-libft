ca5dd235732578e34f5cc898ce3b6c92c83de42f
mmizuki1817 <mmizuki1817@gmail.com>
2022-06-03 03:48:36 +0900

    calloc

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,7 +6,7 @@
 /*   By: mimatsub <mimatsub@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/30 23:38:26 by mimatsub          #+#    #+#             */
-/*   Updated: 2022/05/30 21:01:34 by mimatsub         ###   ########.fr       */
+/*   Updated: 2022/06/03 03:48:01 by mimatsub         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,8 @@ void	*ft_calloc(size_t n, size_t size)
 	if (size != 0 && SIZE_MAX / size < n)
 		return (NULL);
 	ms = n * size;
+	if (ms == 0)
+		ms = 1;
 	buf = (void *)malloc(ms);
 	if (buf == NULL)
 		return (NULL);
